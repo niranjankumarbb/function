26d5ada5af69d65fcde8767f6d7b1c6675f50a80
Niranjan Kumar <niranjan32@gmail.com>
2021-09-24 14:56:01 +0530

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -2,6 +2,6 @@ function welcome(){
 console.info('Hello world')
 console.log('commit in github directly')
 console.log('edited in branch3 branch')
-console.log('this is master branch')
+console.log('this is master branch, edited in master branch only')
 }
 welcome()
