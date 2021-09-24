75acb85eb46dbf3de24e66d8e8a3b0a93a0f84a1
Niranjan Kumar <niranjan32@gmail.com>
2021-09-24 11:35:08 +0530

    updated branch3

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 function temp(){
     console.log('hello world')
-    console.log('edited function file directly in github')
-    console.log('second edit of function file directly in github')
+    console.log('edited  in branch3')
+    console.log('... ')
     
 }
 temp()
--- welcome.js
+++ welcome.js
@@ -1,7 +1,7 @@
 function welcome(){
 console.info('Hello world')
-console.log('commit in github directly')
-console.log('edited in branch3 branch')
-console.log('second commit in github directly in master branch')
+console.log('... ')
+console.log('... ')
+console.log('...')
 }
 welcome()
